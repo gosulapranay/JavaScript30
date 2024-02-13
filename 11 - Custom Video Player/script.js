340d0af24e49c37d6fb65f406b9596dec91c8ceb
gosulapranay <gosulapranay@gmail.com>
2024-02-14 00:18:34 +0530

    added Full Screen Button

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -3,6 +3,7 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
+const full = player.querySelector(".fullscreen");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
@@ -31,6 +32,9 @@ function scrub(e) {
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
 }
+function fullScreen(){
+    video.requestFullscreen();
+}
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -38,6 +42,7 @@ video.addEventListener("pause", updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener("click", togglePlay);
+full.addEventListener("click", fullScreen);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
